fe15c5c6aecbac16f0f7d79bba6b148926acbf00
PetrosB123 <pnblankenstein@willamette.edu>
2025-08-30 22:36:15 -0700

    Initial commit

---
+++ src/main/java/net/riftworks/compostater/CompostaterClient.java
@@ -0,0 +1,10 @@
+package net.riftworks.compostater;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CompostaterClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/riftworks/compostater/CompostaterDataGenerator.java
@@ -0,0 +1,11 @@
+package net.riftworks.compostater;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CompostaterDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
